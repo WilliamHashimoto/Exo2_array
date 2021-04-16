51a75a31176dbfeb221b0fa9267a810f0cc44073
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-16 12:21:00 +0200

    fini

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+let friends = ["jake", "tyson", "nacho"];
+console.log(friends);
+friends.push("mitsu")
+console.log(friends);
+
+//Bonus
+friends.unshift("yuli");
+console.log(friends);
+friends.splice(3, 0, "shinji")
+console.log(friends);
